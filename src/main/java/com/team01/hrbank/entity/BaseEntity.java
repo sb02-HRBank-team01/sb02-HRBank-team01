42a99b55e531c65e9d7282737da370dfda140175
JaeKwan AN <jaekwan99@gmail.com>
2025-04-22 15:39:58 +0900

    Feat: Department 엔티티 및 레포지토리 생성

--- src/main/java/com/team01/hrbank/entity/BaseEntity.java
+++ src/main/java/com/team01/hrbank/entity/BaseEntity.java
@@ -4,13 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.Instant;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
