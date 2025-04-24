d060f350614dad393de8bb0843977d21416226f1
윤영로 <yryun00@gmail.com>
2025-04-24 09:49:16 +0900

    ChangeLogRepositoryCustom파일 수정 중 main으로부터 merge하기 위한 중간 commit

--- src/main/java/com/team01/hrbank/repository/impl/ChangeLogRepositoryImpl.java
+++ src/main/java/com/team01/hrbank/repository/impl/ChangeLogRepositoryImpl.java
@@ -1,5 +1,38 @@
 package com.team01.hrbank.repository.impl;
 
-public class ChangeLogRepositoryImpl implements ChangeLogRepositoryCustom {
+import com.team01.hrbank.repository.ChangeLogRepositoryCustom;
+import org.springframework.data.jpa.repository.query.JpaQueryMethodFactory;
 
+public class ChangeLogRepositoryImpl implements ChangeLogRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<ChangeLog> findAllByConditions(
+        String employeeNumber,
+        ChangeType type,
+        String memo,
+        String ipAddress,
+        Instant atFrom,
+        Instant atTo,
+        Long idAfter,
+        int size,
+        String sortField,
+        String sortDirection
+    ) {
+        // 추후 QueryDSL 조건 작성 예정
+        return List.of(); // 빈 리스트 리턴 (임시)
+    }
+
+    @Override
+    public long countByConditions(
+        String employeeNumber,
+        ChangeType type,
+        String memo,
+        String ipAddress,
+        Instant atFrom,
+        Instant atTo
+    ) {
+        // 추후 QueryDSL 조건 작성 예정
+        return 0L;
+    }
 }
